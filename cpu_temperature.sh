eedab8ae34d3ee532db0b65d3e5123e3fe3babe5
eksplosion <kauandecarli@gmail.com>
2020-08-02 10:42:14 -0300

    makes cpu_temperature.sh work with any core count

--- cpu_temperature.sh
+++ cpu_temperature.sh
@@ -1,4 +1,10 @@
-let "temp0 = $(cat /sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input)"
-let "temp1 = $(cat /sys/devices/platform/coretemp.0/hwmon/hwmon1/temp2_input)"
-let "temp = ($temp0 + $temp1) / 2000"
+let "temps = $(wc -l <<< $(ls /sys/devices/platform/coretemp.0/hwmon/hwmon?/temp*_input))"
+let "total = 0"
+let "i = 1"
+while [ $i -lt $temps ]; do
+    let "temp = $(cat /sys/devices/platform/coretemp.0/hwmon/hwmon?/temp$i\_input)"
+    let "total = $total + $temp"
+    let "i = i + 1"
+done
+let "temp = total / ($i-1) / 1000"
 printf "$temp°C"
